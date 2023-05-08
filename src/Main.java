f386f6daadae4b69bc4d7b52f2065b355a52d0e8
jonathan <jonathan_sm@mail.ru>
2023-05-08 15:23:06 +0300

    домашка 6-2

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("домашка6-2");
+        task1();
+    }
+
+    public static void task1(){
+
+
+
+
+    }
+    }
+
+
+
+    
